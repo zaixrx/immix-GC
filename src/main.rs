9be8a6a6bc247bf8ce104aa0fefc5737ebaaec98
zaixrx <zaizroff@gmail.com>
2026-01-26 19:38:27 +0100

    meaningful example

--- src/main.rs
+++ src/main.rs
@@ -2,23 +2,19 @@ mod tl_alloc;
 mod g_alloc;
 mod block;
 
-use std::ffi::c_char;
 use g_alloc::GlobalAllocator;
 
-unsafe extern "C" {
-    fn printf(fmt: *const c_char, ...);
+#[derive(Debug, Clone)]
+struct Player {
+    name: String,
+    score: usize, 
 }
 
 fn main() {
-    let buf = c"Hello, World";
-    let buf_size = buf.count_bytes();
-
-    let allocator = GlobalAllocator::<c_char>::new();
-    let space = allocator.alloc(buf_size);
-
-    // SAFETY: who gives a fuck
-    unsafe {
-        std::ptr::copy(buf.as_ptr(), space as *mut c_char, buf_size);
-        printf(c"libc_printf: %s\n".as_ptr() as *const c_char, space);
-    }
+    let galloc = GlobalAllocator::<Player>::new();
+    let player_ptr = galloc.alloc(size_of::<Player>()) as *mut Player;
+    let player = unsafe { &mut (*player_ptr) };
+    player.name = String::from("Hello, World!");
+    player.score = 100;
+    dbg!(player);
 }
